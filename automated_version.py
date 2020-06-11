e43cadd695f42beb4033acc337b007130f4d825b
SamHarris2020 <sam.career@outlook.com>
2020-06-10 20:25:49 -0700

    added automatic operating system detection

--- automated_version.py
+++ automated_version.py
@@ -1,16 +1,36 @@
+import csv
 import ctypes
 import json
 import random
+import subprocess
+import sys
 import time
-from os import path, getcwd, makedirs, chdir
+from os import path, getcwd, makedirs, chdir, system
 from urllib import request, error
 
+# Check What Platform We're In
+platforms = {
+    'linux1': 'Linux',
+    'linux2': 'Linux',
+    'darwin': 'OS X',
+    'win32': 'Windows'
+}
+
+platform = platforms[sys.platform]
+
+if sys.platform not in platforms:
+    sys.exit(["Unfortunately, Your Operating System Isn't Supported"])
+
 # Constants & Important Variables
-names = ["happy", "standard", "funny"]
-happy_subreddits = ["aww", "WhatsWrongWithYourDog"]
-standard_subreddits = ["pics", "Images", "wallpaper", "itookapicture", "Art", "generativeart", "generative", "CozyPlaces"]
-funny_subreddits = ["Beans", "delusionalartists", "classicalartmemes"]
-combined = (happy_subreddits, standard_subreddits, funny_subreddits)
+subreddits = []
+
+with open("subreddits.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for lines in csv_reader:
+        # lazy way, didn't want to bother with reading csv column into list
+        subreddits.append(lines[0])
+
+save_all_images = True
 
 # File Path
 file_path = path.join(getcwd(), "image_storage")
@@ -23,7 +43,7 @@ if not path.exists(file_path):
 chdir(file_path)
 
 # Automatically Choose
-choice = random.choice(combined)
+choice = random.choice(subreddits)
 
 # Find Daily Top Post - AFAIK it's magic
 url = "https://www.reddit.com/r/" + choice + "/search.json?q=url%3A.jpg+OR+url%3A.png&sort=top&restrict_sr=on&t=day"
@@ -43,8 +63,44 @@ image_filename = "bg_" + str(int(round(time.time() * 1000))) + ".jpg"
 final_path = path.join(file_path, image_filename)
 open(final_path, "wb").write(request.urlopen(top_post["url"]).read())
 
-path.realpath(final_path)
+final_path = path.realpath(final_path)
 
 # Set Background
-print(path.realpath(final_path))
-ctypes.windll.user32.SystemParametersInfoW(20, 0, path.realpath(final_path), 0)
+# print(path.realpath(final_path))
+
+if platform == "Windows":
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, final_path, 0)
+
+elif platform == "OS X":
+
+    # Mac Stuff Stolen From Here: https://github.com/vegasbrianc/mac-background/blob/master/background.py
+    # Author Brian Christner
+
+    cmd = """/usr/bin/osascript<<END
+    tell application "Finder"
+    set desktop picture to POSIX file "%s"
+    end tell
+    END"""
+
+    subprocess.Popen(cmd % final_path, shell=True)
+    subprocess.call(["killall Dock"], shell=True)
+
+else:  # Linux
+
+    # Linux Stuff Stolen From Here: https://github.com/geekpradd/PyWallpaper
+    # Author Pradipta Bora
+
+    def get_output(command):
+        p = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        return out
+
+
+    try:
+        v = int(get_output("gnome-session --version").split()[-1][0])
+        if v == 2:
+            system("gconftool-2 --type=string --set /desktop/gnome/background/picture_filename {0}".format(final_path))
+        elif v == 3:
+            system('gsettings set org.gnome.desktop.background picture-uri "file://{0}"'.format(final_path))
+    except error:
+        raise OSError("Wallpaper Change is supported in GNOME 2/3 and Unity only")
